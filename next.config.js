cad39e846892b0e81ee1371712cc381ccd3651e1
sainibharat277@gmail.com <bharatgknext@gmail.com>
2026-06-26 17:57:12 +0530

    update next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { serverActions: { bodySizeLimit: "1mb" } },
-  images: { domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"] },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "1mb",
+    },
+  },
+
+  images: {
+    domains: [
+      "avatars.githubusercontent.com",
+      "lh3.googleusercontent.com",
+    ],
+  },
+
+  // 👇 IMPORTANT FIX
+  turbopack: {},
+
   webpack: (config) => {
-    config.experiments = { ...config.experiments, asyncWebAssembly: true };
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+    };
     return config;
   },
 };
+
 module.exports = nextConfig;
